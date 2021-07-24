7579b3c4cdf430550dc52976d5f934c57fd07f44
Rares Cruceat <cruceat.rares@gmail.com>
2021-07-24 07:23:49 +0300

    create routes for controllers

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
 
-class Buyer extends Model
+class Buyer extends User
 {
     use HasFactory;
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +12,33 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+ * Buyers
+*/
+Route::resource('buyers', 'App\Http\Controllers\Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/**
+ * Sellers
+*/
+Route::resource('sellers', 'App\Http\Controllers\Seller\SellerController', ['only' => ['index', 'show']]);
+
+/**
+ * Categories
+*/
+Route::resource('categories', 'App\Http\Controllers\Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/**
+ * Products
+*/
+Route::resource('products', 'App\Http\Controllers\Product\ProductController', ['only' => ['index', 'show']]);
+
+/**
+ * Transaction
+*/
+Route::resource('transactions', 'App\Http\Controllers\Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+/**
+ * Users
+*/
+Route::resource('users', 'App\Http\Controllers\User\UserController', ['except' => ['create', 'edit']]);
