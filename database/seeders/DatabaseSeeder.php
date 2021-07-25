ae7ad0c361b90250988ac976abd93d860183cf28
Rares Cruceat <cruceat.rares@gmail.com>
2021-07-25 10:12:01 +0300

    implement database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +19,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Transaction::truncate();
+        Category::truncate();
+        Product::truncate();
+        DB::table('category_product')->truncate();
+
+        $usersQuantity = 200;
+        $categoriesQuantity = 30;
+        $productsQuantity = 1000;
+        $transactionsQuantity = 1000;
+
+        User::factory()->count($usersQuantity)->create();
+        Category::factory()->count($categoriesQuantity)->create();
+        Product::factory()->count($productsQuantity)->create()->each(
+            function($product) {
+                $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+                $product->categories()->attach($categories);
+            }
+        );
+        Transaction::factory()->count($transactionsQuantity)->create();
     }
 }
